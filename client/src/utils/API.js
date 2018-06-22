beeef8ba469f32c02e9c3c679adc48b1fc06b493
William Young <williamcorinyoung@gmail.com>
2018-06-22 11:47:26 -0700

    modify searchbar js for api integration

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,13 +1,14 @@
 import axios from "axios";
 
-const USERSEARCH = "happy%20hour";
+// const USERSEARCH = "bar";
 const LATLONG = "@47.6918452,-122.2226413";
 const APIKEY = "AIzaSyD_2mmRZkUnIuOqeIxJRjKZjDadVGB1i0E";
 
 export default {
   // Place search API call
-  getPlaces: function () {
-    return axios.get("https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=" + USERSEARCH + "&inputtype=textquery&fields=photos,formatted_address,name,opening_hours,rating&locationbias=circle:2000" + LATLONG + "&key=" + APIKEY);
+  getPlaces: function (query) {
+    alert("Hey you! You've succesfully queried the google api. Good on ya.");
+    return axios.get("https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=" + query + "&inputtype=textquery&fields=photos,formatted_address,name,opening_hours,rating&locationbias=circle:2000" + LATLONG + "&key=" + APIKEY);
   },
   // Gets all businesses
   getBusinesses: function () {
