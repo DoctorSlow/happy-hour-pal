0be8a9b0f3894e49b12eab29ad5c8e3ed5dc03c3
mtking78 <mtking78@gmail.com>
2018-06-23 14:47:16 -0700

    Deal card pulls from Mongo via API call

--- client/src/pages/BusinessDetails/BusinessDetails.js
+++ client/src/pages/BusinessDetails/BusinessDetails.js
@@ -1,81 +1,87 @@
 import React, { Component } from "react";
 import { Card, Row, Col } from 'reactstrap';
-// import API from "../utils/API";
-import deals from "../../deals.json";
+// import deals from "../../deals.json";
+// import Deals from "./Deals";
 import reviews from "../../reviews.json";
-import { BusinessCard, BusinessNameCard, DealCard } from "../../components/Business";
+import { BusinessCard, BusinessNameCard, DealCard, Deals } from "../../components/Business";
 import { ReviewCard, ReviewsContainer } from "../../components/Reviews";
 // import ReviewCard from "../../components/ReviewCard";
 // import ReviewsContainer from "../../components/ReviewsContainer";
 // import { BackBtn, ReportDealBtn, RateDealBtn, ViewAllBtn, SuggestEditBtn } from "../../../components/Buttons";
 
-
 class Results extends Component {
   state = {
     reviews,
-    deals,
+    // deals,
     name: "Ermanos",
     address: "http://www.ermanosbrew.com/",
     stars: "****"
-
-   
   };
 
-  
-
-
-
   render() {
-
     return (
       <div>
         <Row>
-  <Col sm="4">
-        {/* <BackBtn /> */}
-        <BusinessCard>
-             <BusinessNameCard 
-             name={this.state.name} address={this.state.address} stars={this.state.stars} 
-             />
+          <Col sm="4">
 
-      {this.state.deals.map(deal => (   
-       <DealCard
-              // onClick={() => this.handleClickEvent(pic.id)}
-              id={deal.id}
-              key={deal.id}
-              day={deal.day}
-              beginTime={deal.beginTime}
-              endTime={deal.endTime}
-              info={deal.info}
-       />
-          ))}
+            {/* <BackBtn /> */}
 
-        </BusinessCard>
-        {/* <ReportDealBtn /> */}
-        {/* <RatingCard>
-            {children}
-        </RatingCard> */}
-        <ReviewsContainer>
-            {this.state.reviews.map(review => (   
-       <ReviewCard
-              // onClick={() => this.handleClickEvent(pic.id)}
-              id={review.id}
-              key={review.id}
-              username={review.username}
-              date={review.date}
-              info={review.info}
-             
-       />
-          ))}
-        </ReviewsContainer>
-        {/* <TopPalCard>
-            {children}
-        </TopPalCard> */}
-        {/* <SuggestEditBtn /> */}
-        </Col>
+            <BusinessCard>
+
+              <BusinessNameCard
+                name={this.state.name}
+                address={this.state.address}
+                stars={this.state.stars}
+              />
+
+              {/* New Deal Card Component*/}
+              <Deals />
+
+              {/* Old Deal Card logic reading deals.json */}
+              {/* {this.state.deals.map(deal => (
+                <DealCard
+                  // onClick={() => this.handleClickEvent(pic.id)}
+                  id={deal.id}
+                  key={deal.id}
+                  day={deal.day}
+                  beginTime={deal.beginTime}
+                  endTime={deal.endTime}
+                  info={deal.info}
+                />
+              ))} */}
+
+            </BusinessCard>
+
+            {/* <ReportDealBtn /> */}
+
+            {/* <RatingCard>
+              {children}
+            </RatingCard> */}
+
+            <ReviewsContainer>
+              {this.state.reviews.map(review => (
+                <ReviewCard
+                  // onClick={() => this.handleClickEvent(pic.id)}
+                  id={review.id}
+                  key={review.id}
+                  username={review.username}
+                  date={review.date}
+                  info={review.info}
+                />
+              ))}
+            </ReviewsContainer>
+
+            {/* <TopPalCard>
+              {children}
+            </TopPalCard> */}
+
+            {/* <SuggestEditBtn /> */}
+
+          </Col>
         </Row>
       </div>
     );
   }
 }
 
-export default Results; 
+export default Results;
