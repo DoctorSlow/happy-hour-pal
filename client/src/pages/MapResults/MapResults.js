ac245a72c76703a320e156a84389968f7de69dbd
William Young <williamcorinyoung@gmail.com>
2018-06-24 19:34:35 -0700

    switching

--- client/src/pages/MapResults/MapResults.js
+++ client/src/pages/MapResults/MapResults.js
@@ -1,19 +1,54 @@
-import React from "react";
+import React, { Component } from "react";
 // import Container from "../../components/Container";
 // import Row from "../../components/Row";
 // import Col from "../../components/Col";
 import SearchBar from "../../components/SearchBar";
+import API from "../../utils/API";
 import "./MapResults.css";
 
-//refactor to class, give searchbar a onSearch function, after the data has been grabbed pass that data to the map 
-const MapResults = () => (
-  // handleSearch() {
+//this component will be refactored to contain a method that stores state for google api results 
 
-  // }
-  <div className="mapHeight">
-    <SearchBar onSearch={this.handleSearch} />
-    <div className="mapHeight" id="map"></div>
-  </div>
-);
+class MapResults extends Component {
+
+  state = {
+    results: [],
+    search: ""
+  };
+
+  //allows state changes(right now just the search parameter) to be updated live
+  handleInputChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handleSearchSubmit = event => {
+    event.preventDefault();
+    this.searchGoogle(this.state.search);
+  };
+
+  searchGoogle(query) {
+    console.log("google has been searched")
+    API.getPlaces(query)
+      .then(res =>
+        this.setState({ results: res.data.results })
+        // if(this.props.onSearch) {
+        //     this.props.onSearch(res.data.results);
+        // }
+      )
+      .catch(err => console.log(err));
+  };
+
+  render() {
+
+    return (
+      <div className="mapHeight">
+        <SearchBar onClick={this.handleSearchSubmit} onChange={this.handleInputChange} />
+        <div className="mapHeight" id="map"></div>
+      </div>
+    )
+  }
+}
 
 export default MapResults;
