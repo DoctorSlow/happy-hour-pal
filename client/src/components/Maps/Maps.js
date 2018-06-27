c507242b86ee2f22e57266efff89ddbb859ff3eb
Jeremy J. Lee <doctorslow@gmail.com>
2018-06-26 20:38:56 -0700

    map rendering with React now

--- client/src/components/Maps/Maps.js
+++ client/src/components/Maps/Maps.js
@@ -1,44 +1,15 @@
 // maintain a state on the parent to pass down to both listresults and mapresults pages
-
 import React, { Component } from 'react';
-import { Link } from "react-router-dom";
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
 import "./Maps.css";
-import API from "../../utils/API";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
+const MyMapComponent = withScriptjs(withGoogleMap((props) =>
+  <GoogleMap
+    defaultZoom={13}
+    defaultCenter={{ lat: -34.397, lng: 150.644 }}
+  >
+    {props.isMarkerShown && <Marker position={{ lat: -34.397, lng: 150.644 }} />}
+  </GoogleMap>
+))
 
-    // Must initialize state first
-    this.state = { count: 0 };
-  }
-
-  handleClick() {
-    // Increment the count when the button is clicked
-    this.setState({
-      count: this.state.count + 1
-    }, function () {
-      // setState is asynchronous! This function gets called
-      // when it's finished.
-      console.log("Job's done");
-    });
-  }
-
-  render() {
-    return (
-      <div className="app">
-        <div className="click-count">
-          Button presses: {this.state.count}
-        </div>
-        <button onClick={this.handleClick.bind(this)}>
-          Add One
-        </button>
-      </div>
-    );
-  }
-}
-
-ReactDOM.render(
-  <App />,
-  document.getElementById('container')
-);
+export default MyMapComponent;
--- client/src/pages/MapResults/MapResults.js
+++ client/src/pages/MapResults/MapResults.js
@@ -1,61 +1,25 @@
 import React, { Component } from "react";
-import { GoogleMap, Marker } from "react-google-maps"
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
 // import Container from "../../components/Container";
 // import Row from "../../components/Row";
 // import Col from "../../components/Col";
 import SearchBar from "../../components/SearchBar";
+import MyMapComponent from "../../components/Maps"
 import "./MapResults.css";
 
 class MapResults extends Component {
-
-  // Note: This example requires that you consent to location sharing when
-  // prompted by your browser. If you see the error "The Geolocation service
-  // failed.", it means you probably did not give permission for the browser to
-  // locate you.
-  //   var map, infoWindow;
-  // function initMap() {
-  //   map = new google.maps.Map(document.getElementById('map'), {
-  //     center: { lat: -34.397, lng: 150.644 },
-  //     zoom: 13
-  //   });
-  //   infoWindow = new google.maps.InfoWindow;
-
-  //   // Try HTML5 geolocation.
-  //   if (navigator.geolocation) {
-  //     navigator.geolocation.getCurrentPosition(function (position) {
-  //       var pos = {
-  //         lat: position.coords.latitude,
-  //         lng: position.coords.longitude
-  //       };
-
-  //       infoWindow.setPosition(pos);
-  //       infoWindow.setContent('Nearby happy hours coming soon, pal.');
-  //       infoWindow.open(map);
-  //       map.setCenter(pos);
-  //     }, function () {
-  //       handleLocationError(true, infoWindow, map.getCenter());
-  //     });
-  //   } else {
-  //     // Browser doesn't support Geolocation
-  //     handleLocationError(false, infoWindow, map.getCenter());
-  //   }
-  // }
-
-  // function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-  //   infoWindow.setPosition(pos);
-  //   infoWindow.setContent(browserHasGeolocation ?
-  //     'Error: The Geolocation service failed.' :
-  //     'Error: You must enable Geolocation services to use this app');
-  //   infoWindow.open(map);
-  // }
-
-
-
 
   render() {
     return (
       <div className="mapHeight">
         <SearchBar />
+        < MyMapComponent
+          isMarkerShown
+          googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyD_2mmRZkUnIuOqeIxJRjKZjDadVGB1i0E"
+          loadingElement={<div style={{ height: `100%` }} />}
+          containerElement={<div style={{ height: `100%` }} />}
+          mapElement={<div style={{ height: `100%` }} />}
+        />
         <div className="mapHeight" id="map"></div>
       </div>
     )
