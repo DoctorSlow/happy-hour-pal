223891875580751c2d9999aa7a6cbf4d21ef57b0
William Young <williamcorinyoung@gmail.com>
2018-06-26 21:18:38 -0700

    fixed cors query

--- controllers/mapsController.js
+++ controllers/mapsController.js
@@ -1,9 +1,9 @@
 const axios = require('axios');
 
-const LATLONG = "47.6918452,-122.2226413";
+const LATLONG = "32.2226,-110.9747";
 const APIKEY = "AIzaSyD_2mmRZkUnIuOqeIxJRjKZjDadVGB1i0E";
 
-// https://maps.googleapis.com/maps/api/place/nearbysearch/json?keyword=beer&location=-33.8670522,151.1957362&radius=15000&type=restaurant&keyword=cruise&key=AIzaSyD_2mmRZkUnIuOqeIxJRjKZjDadVGB1i0E
+//https://maps.googleapis.com/maps/api/place/nearbysearch/json?keyword=beer&location=32.2226,-110.9747&radius=15000&key=AIzaSyD_2mmRZkUnIuOqeIxJRjKZjDadVGB1i0E
 
 module.exports = {
 	// (trying to set the LATLONG const. to the the User location via geolocation)
@@ -15,7 +15,8 @@ module.exports = {
 	// 		})
 	// },
 	queryMap: function (req, res) {
-		axios.get("https://maps.googleapis.com/maps/api/place/nearbysearch/json?keyword=" + req.params.query + "&location=" + LATLONG + "&radius=1500&key=" + APIKEY)
+		axios.get("https://maps.googleapis.com/maps/api/place/nearbysearch/json?keyword=" + req.params.query + "&location=" + LATLONG + "&radius=15000&key=" + APIKEY)
+
 			.then((data) => {
 				console.log(data.data);
 				res.json(data.data)
