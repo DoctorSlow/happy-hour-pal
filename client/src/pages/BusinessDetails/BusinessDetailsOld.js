edd2e47f713dfe208e318e9df0f07326673d8071
mtking78 <mtking78@gmail.com>
2018-06-28 19:48:26 -0700

    New models added, not implemented yet.

--- client/src/pages/BusinessDetails/BusinessDetailsOld.js
+++ client/src/pages/BusinessDetails/BusinessDetailsOld.js
@@ -8,87 +8,41 @@ import API from "../../utils/API";
 // import ReviewCard from "../../components/ReviewCard";
 // import ReviewsContainer from "../../components/ReviewsContainer";
 // import { BackBtn, ReportDealBtn, RateDealBtn, ViewAllBtn, SuggestEditBtn } from "../../../components/Buttons";
-import { List, ListItem } from "../../components/List";
-import { FormBtn } from "../../components/Form";
-import SearchForm from "../../components/SearchForm";
 
 class Results extends Component {
   state = {
-    business: [],
     businesses: [],
     day: "",
     beginTime: "",
     endTime: "",
     info: "",
-    reviews,
+    reviews: [],
     visibility: "hidden",
-    name: "Ermanos",
-    address: "http://www.ermanosbrew.com/",
-    stars: "****",
-    search: "",
-    currentBusiness: []
+    name: "",
+    address: "",
+    stars: "",
   };
 
   componentDidMount() {
-    this.loadAllBusinesses()
+    this.loadBusinesses();
   }
 
-  // Retrieve all businesses to be displayed in select drop-down.
-  loadAllBusinesses = () => {
+  loadBusinesses = () => {
     API.getBusinesses()
       .then(res =>
         this.setState({
           businesses: res.data,
+          name: "The Shanty",
           day: "",
           beginTime: "",
           endTime: "",
-          info: "",
-          // currentBusiness: []
+          info: ""
         })
       )
       .catch(err => console.log(err));
-  }
-
-  // Upon select, get info of selected business (by id).
-  loadTargetBusiness = (event) => {
-    const select = event.target;
-    const selectedOption = select[select.selectedIndex];
-    API.getBusiness(selectedOption.getAttribute('value'))
-    .then(res =>{
-      this.setState({currentBusiness: res.data});
-    })
-  }
-
-  // handleSelectedOption = event => {
-  //   this.setState({ search: event.target.value });
-  // };
-  handleSelectedOption = event => {
-    this.setState({
-      [event.target.name]: event.target.value
-    });
   };
-  // Old way to get single business when search form is on same page.
-  // // Get back the info from a single business, by its id
-  // handleFormSubmit = event => {
-  //   event.preventDefault();
-  //   API.getBusiness(this.state.search)
-  //   .then(res =>
-  //     this.setState({
-  //       businesses: res.data,
-  //       day: "",
-  //       beginTime: "",
-  //       endTime: "",
-  //       info: ""
-  //     })
-  //   )
-  //     .catch(err => this.setState({ error: err.message }));
-  // };
 
   render() {
-
-    // Option A ... avoids declaring initially in state.
-    const {currentBusiness} = this.state
-
     return (
       <div>
         <Row>
@@ -104,11 +58,11 @@ class Results extends Component {
                 stars={this.state.stars}
               />
 
-              {/* Deal Card displays all data from business collection. */}
               {this.state.businesses.map(business => (
                 <DealCard
                   // onClick={() => this.handleClickEvent(pic.id)}
                   id={business._id}
+                  name={this.state.name}
                   key={business._id}
                   day={business.day}
                   beginTime={business.beginTime}
@@ -146,30 +100,6 @@ class Results extends Component {
             </TopPalCard> */}
 
             {/* <SuggestEditBtn /> */}
-
-          </Col>
-          <Col sm="4">
-
-            {/* Component for drop-down business list. */}
-            <SearchForm
-              handleSelectedOption={this.handleSelectedOption}
-              businesses={this.state.businesses}
-              loadTargetBusiness={this.loadTargetBusiness}
-            />
-
-            {/* Option A ... Div to display business selected from SearchForm */}
-            {currentBusiness &&
-              <div>
-                <h1>{currentBusiness.name}</h1>
-                <h2>{currentBusiness._id}</h2>
-              </div>
-            }
-
-            {/* Option B ... uses set state */}
-            {/* <div>
-              <h1>{this.state.currentBusiness.name}</h1>
-              <h2>{this.state.currentBusiness._id}</h2>
-            </div> */}
 
           </Col>
         </Row>
