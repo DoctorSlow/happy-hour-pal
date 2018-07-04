ff196ffed56f9b838456089430462288965aae22
lanae <rlfoard@gmail.com>
2018-07-03 20:50:08 -0700

    added quail icon map markers

--- client/src/components/Maps/Maps.js
+++ client/src/components/Maps/Maps.js
@@ -5,7 +5,6 @@ import "./Maps.css";
 //The two ternary functions handle our geolocation coords, if they exist, set map center to that, if not
 //default to downtown tucson
 const MyMapComponent = withScriptjs(withGoogleMap(function (props) {
-<<<<<<< HEAD
   console.log(props);
   let iconMarker = new window.google.maps.MarkerImage(
     '/assets/images/quailpointerwhite.png',
@@ -14,9 +13,6 @@ const MyMapComponent = withScriptjs(withGoogleMap(function (props) {
     null, /* anchor is bottom center of the scaled image */
     new window.google.maps.Size(28.9364, 39)
   );
-=======
-
->>>>>>> fc8571159b6bd9b43f9f59a0530cea4a34c41646
   return < GoogleMap
     defaultZoom={13.5}
     defaultCenter={props.center ? { lat: props.center.lat, lng: props.center.lng } : { lat: 32.2226, lng: -110.974 }}
