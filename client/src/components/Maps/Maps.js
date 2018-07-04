92ecbd12f0cfdd995669485bcd2f6f4504b4df26
lanae <rlfoard@gmail.com>
2018-07-03 20:47:12 -0700

    add quail icon map markers

--- client/src/components/Maps/Maps.js
+++ client/src/components/Maps/Maps.js
@@ -5,6 +5,13 @@ import "./Maps.css";
 
 const MyMapComponent = withScriptjs(withGoogleMap(function (props) {
   console.log(props);
+  let iconMarker = new window.google.maps.MarkerImage(
+    '/assets/images/quailpointerwhite.png',
+    null, /* size is determined at runtime */
+    null, /* origin is 0,0 */
+    null, /* anchor is bottom center of the scaled image */
+    new window.google.maps.Size(28.9364, 39)
+  );
   return < GoogleMap
     defaultZoom={13.5}
     defaultCenter={props.center ? { lat: props.center.lat, lng: props.center.lng } : { lat: 32.2226, lng: -110.974 }}
@@ -14,6 +21,8 @@ const MyMapComponent = withScriptjs(withGoogleMap(function (props) {
     {
       props.results.map(mark => (
         <Marker
+
+          icon={iconMarker}
           position={{ lat: mark.geometry.location.lat, lng: mark.geometry.location.lng }}
           title={mark.name}
           place_id={mark.place_id}
