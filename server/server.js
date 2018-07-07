a2ac68adf51ef0003d9ec38a77305c0599d33503
Jeremy J. Lee <doctorslow@gmail.com>
2018-07-07 15:19:02 -0700

    adding build

--- server/server.js
+++ server/server.js
@@ -1,8 +1,4 @@
-// Loading evnironmental variables here
-if (process.env.NODE_ENV !== 'production') {
-	console.log('loading dev environments')
-	require('dotenv').config()
-}
+
 require('dotenv').config()
 
 // Dependencies
