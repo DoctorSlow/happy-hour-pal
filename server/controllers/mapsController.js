7def4b5cdfb74b305db44fa10ab17157fce8196c
William Young <williamcorinyoung@gmail.com>
2018-07-07 14:13:48 -0700

    extended google auto search api results

--- server/controllers/mapsController.js
+++ server/controllers/mapsController.js
@@ -9,18 +9,36 @@ module.exports = {
 
 	queryMap: function (req, res) {
 
-		axios.get("https://maps.googleapis.com/maps/api/place/nearbysearch/json?keyword=" + req.params.query + "&location=" + req.params.lat + "," + req.params.lng + "&radius=3200&key=" + APIKEY)
+		axios.get("https://maps.googleapis.com/maps/api/place/nearbysearch/json?keyword=" + req.params.query + "&location=" + req.params.lat + "," + req.params.lng + "&radius=7500&key=" + APIKEY)
 
 			.then((data) => {
 				res.json(data.data);
 			})
 	},
+	//&pagetoken=next_page_token
 	autoQuery: function (req, res) {
-		axios.get("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + req.params.lat + "," + req.params.lng + "&radius=3200&type=bar&key=" + APIKEY)
+		axios.get("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + req.params.lat + "," + req.params.lng + "&radius=7500&type=bar&key=" + APIKEY)
 
 			.then((data) => {
-				// console.log(data.data);
-				res.json(data.data);
+				let firstQuery = data.data;
+
+				let pageToken = data.data.next_page_token;
+				console.log(pageToken);
+				//this request needs time before the page token is validated 
+				setTimeout(function () {
+					axios.get("https://maps.googleapis.com/maps/api/place/nearbysearch/json?pagetoken=" + pageToken + "&key=" + APIKEY)
+
+						.then((results) => {
+							let secondQuery = results.data;
+							// var compiled = extend(firstQuery, secondQuery);
+							// console.log(secondQuery);
+							var compiled = {
+								status: "OK",
+								results: [...firstQuery.results, ...secondQuery.results]
+							};
+							res.json(compiled);
+						})
+				}, 2000);
 			})
 	}
 
